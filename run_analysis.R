7a6bb95fc7c19171c2579f3b7ab46ee0a9ebb317
Alexis de la Cotte <alexis.delacotte@gmail.com>
2021-01-07 14:24:28 +0100

    new R code and txt files

--- run_analysis.R
+++ run_analysis.R
@@ -30,16 +30,19 @@ features_names <- features$V2
 mstd <- grep("(mean|std)",features_names)
 mstd_names <- features_names[mstd]
 
-mstd_names<-gsub("Acc", "Accelerometer_", mstd_names)
-mstd_names<-gsub("Gyro", "Gyroscope_", mstd_names)
-mstd_names<-gsub("BodyBody", "Body", mstd_names)
-mstd_names<-gsub("Mag", "Magnitude_", mstd_names)
-mstd_names<-gsub("tBody", "TimeBody_", mstd_names)
-mstd_names<-gsub("-mean()", "Mean", mstd_names, ignore.case = TRUE)
-mstd_names<-gsub("-std()", "STD", mstd_names, ignore.case = TRUE)
-mstd_names<-gsub("-freq()", "_Frequency", mstd_names, ignore.case = TRUE)
-mstd_names<-gsub("fBody", "FrequencyBody_", mstd_names)
-mstd_names<-gsub("tGravity", "TimeGravity_", mstd_names)
+
+mstd_names <- gsub("Acc", "Accelerometer_", mstd_names)
+mstd_names <- gsub("Gyro", "Gyroscope_", mstd_names)
+mstd_names <- gsub("BodyBody", "Body", mstd_names)
+mstd_names <- gsub("Mag", "Magnitude_", mstd_names)
+mstd_names <- gsub("tBody", "TimeBody_", mstd_names)
+mstd_names <- gsub("-mean()", "Mean", mstd_names)
+mstd_names <- gsub("-std()", "STD", mstd_names)
+mstd_names <- gsub("-freq()", "_Frequency", mstd_names)
+mstd_names <- gsub("fBody", "FrequencyBody_", mstd_names)
+mstd_names <- gsub("tGravity", "TimeGravity_", mstd_names)
+mstd_names <- gsub("()","",mstd_names,fixed=TRUE)
+
 
 write.table(mstd_names,'selected_features.txt',row.name=FALSE, col.names = FALSE)
 
